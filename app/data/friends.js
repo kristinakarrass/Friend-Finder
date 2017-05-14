3d4951c4fa43aa437abece5c5600a7637c11e005
Kristina Karrass <kristinakarrass@gmail.com>
2017-05-14 14:33:59 -0400

    routes

---
+++ app/data/friends.js
@@ -0,0 +1,17 @@
+var friendArray = [
+{
+	friendName: "John",
+	photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwie_tnCgvDTAhWCeCYKHR2tB4cQjRwIBw&url=http%3A%2F%2Fwww.apropos-foto.de%2Fbewerbungsfotos-berlin&psig=AFQjCNELfyZ2aTfcVipQ4gRJZU0c6JTDXg&ust=1494873108704594",
+	scores: [	3,
+				5,
+				2,
+				4,
+				1,
+				4,
+				3,
+				5,
+				2,
+				3]
+}];
+
+module.exports = friendArray;
