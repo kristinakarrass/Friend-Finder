bcd75cec9ebab1b15c67bc092161495f431ffc04
Kristina Karrass <kristinakarrass@gmail.com>
2017-05-14 11:22:53 -0400

    routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,10 +1,19 @@
+var path = require("path");
 
+//=====================================================================
 
-app.get('/survey', function(req, res){
-	res.sendFile(path.join(__dirname, "../public/survey.html"));
-});
+module.exports = function(app) {
+	//sends user to survey page
+    app.get('/survey', function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+    //sends user to homepage
+    app.use("/home", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
+    //if no path is specified, send user to homepage
+    app.use( function(req, res) {
+    	res.sendFile(path.join(__dirname, "../public/home.html"));
+    })
 
-app.use("/home", function(req, res) {
-	res.sendFile(path.join(__dirname, "../public/home.html"));
-});
-
+}
