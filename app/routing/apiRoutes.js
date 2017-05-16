11c2e594501c0e0aef29254f305af11e0fb265a9
Kristina Karrass <kristinakarrass@gmail.com>
2017-05-16 14:49:53 -0400

    logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,13 +5,14 @@ var friendsArray = require("../data/friends.js");
 module.exports = function(app) {
 	//api route handles when user visits page
     app.get("/api/friends", function(req, res) {
-        res.json(friends);
+        res.json(friendsArray);
     });
     //api route handles submitted form by user by pushing it to Friends Array
-    app.post("/api/friends", function(req, res) {
+    app.post("/api/new", function(req, res) {
         var newFriend = req.body;
-        // newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
         console.log(newFriend);
+//add matching logic here and send info to modal on survey.html page
+
         friends.push(newFriend);
         // res.json(newFriend);
     });
