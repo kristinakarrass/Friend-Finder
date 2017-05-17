cbcfe9a5a37b491cdd86427f44e42dbd2089d837
Kristina Karrass <kristinakarrass@gmail.com>
2017-05-17 11:40:52 -0400

    css

--- app/data/survey.js
+++ app/data/survey.js
@@ -26,7 +26,7 @@ function createSurvey() {
         question.append(title);
         var text = $("<h3>").html(questions[i - 1]);
         question.append(text);
-        var answer = $("<select class='form-control' id='answer" + i + "'><option></option><option>1 (Strongly Disagree)</option>" +
+        var answer = $("<select class='form-control' id='answer" + i + "'>Select an answer<option></option><option>1 (Strongly Disagree)</option>" +
             "<option>2</option><option>3</option><option>4</option><option>5 (Strongly Agree)</option>");
         question.append(answer);
         question.append("<hr>");
@@ -74,7 +74,7 @@ $("#submit").on("click", function() {
         for (var i = 0; i < data.length; i++) {
             //append each match's information to modal
             $(".modal-body").append("<h2 class='text-center'>Name: " + data[i].name + "</h2>");
-            $(".modal-body").append("<img src='" + data[i].photo + "'>");
+            $(".modal-body").append("<img src='" + data[i].photo + "' class='img-thumbnail'>");
         }
         //make modal visible
         $('#matchModal').modal('toggle');
